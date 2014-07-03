954201455992bb707ff596e78b4f40811351330a
7flying <diez.f.irene@gmail.com>
2014-07-03 23:44:47 +0200

    command line options defined

--- sachan/sachan.py
+++ sachan/sachan.py
@@ -2,6 +2,8 @@
 from os import listdir
 from os.path import isfile
 import hashlib
+from sys import argv
+import getopt
 
 def gethashfile(path_to_file):
 	f = open(path_to_file, 'rb')
@@ -10,9 +12,24 @@ def gethashfile(path_to_file):
 	f.close()
 	return sha.hexdigest()
 
-def main():
-	path_to_file = r'F:\dump\pruebas\sachan\file.txt'
-	print gethashfile(path_to_file)
+def savechanges(backupdir, sourcedir):
+	pass
+
+def main(argv):
+	option, args = getopt.getopt(argv, "hb:s:")
+	backupdir = ''
+	sourcedir = ''
+	for opt, arg in option:
+		if opt == '-h':
+			print 'sachan.py -b <backupdir> -s <sourcedir> '
+		elif opt == '-b':
+			backupdir = arg
+		elif opt == '-s':
+			sourcedir = arg
+	savechanges(backupdir, sourcedir)
+	
+	#path_to_file = r'F:\dump\pruebas\sachan\file.txt'
+	#print gethashfile(path_to_file)
 
 if __name__ == '__main__':
-	main()
+	main(argv[1:])
